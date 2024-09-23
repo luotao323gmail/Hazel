3900d88630d8b1320329727278bacdab2b9334c8
luotao <luotao323@gmail.com>
2024-09-24 00:04:17 +0900

    commit;

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -15,3 +15,15 @@
 #include "Hazel/EntryPoint.h"
 
 #include "Hazel/Vector2D.h"
+
+
+//ID	Author	Date	Message
+//8915f7933a746430ec715282bfb265336f1772e4	omar < omarcornut@gmail.com>	2019 / 02 / 24 22:24 : 07 + 00 : 00	Merge branch 'master' into docking
+//
+//
+//ID	Author	Date	Message
+//87ded34f9f7f13f99130c171632bc8e21b857d62	omar < omarcornut@gmail.com>	2019 / 02 / 22 11 : 29 : 18 + 00 : 00	Merge branch 'master' into docking
+//
+//
+//ID	Author	Date	Message
+//cf5a93ac37ff393bb5c308b5bd93347eb90629b3	TheCherno < y@nchernikov.com>	2019 / 02 / 09 0:59 : 05 + 00 : 00	Added premake file.
